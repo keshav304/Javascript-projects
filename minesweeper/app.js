63f7547ba58b86b64d4d4aa72a09278987268f88
keshav jha <keshavjhaa2678@gmail.com>
2020-08-19 10:18:22 +0530

    Update app.js

--- minesweeper/app.js
+++ minesweeper/app.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const bombsArray = Array(bombAmount).fill('bomb')
         const emptyArray = Array(width * width - bombAmount).fill('valid')
         const gameArray = emptyArray.concat(bombsArray)
+        //main shuffling
         const shuffledArray = gameArray.sort(() => Math.random() - 0.5)
 
         for (let i = 0; i < width * width; i++) {
@@ -44,6 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const isRightEdge = (i % width === width - 1)
 
             if (squares[i].classList.contains('valid')) {
+                //checking for bombs in all the possible directions
                 if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total++
                 if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) total++
                 if (i > 10 && squares[i - width].classList.contains('bomb')) total++
